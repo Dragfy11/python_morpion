07e0f0ddc2d396b65cae62118fe8df93c95268b5
Dragfy11 <dragfy_dark@hotmail.com>
2021-04-30 13:17:41 +0200

    début jeu

---
+++ morpion.py
@@ -0,0 +1,15 @@
+class morpion:
+    def __init__(self):
+        self.table_jeu   = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.joueur = ""
+        self.ordi   = ""
+
+        print("\n\t jeu morpion")
+        print("\t--------------------")
+        print("\n\n")
+
+    
+
+# Jeu
+# if __name__ == "__main__":
+    
